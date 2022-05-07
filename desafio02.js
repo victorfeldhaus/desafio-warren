11a2222689aa4c36da46c5791d194edd291d610a
Victor Feldhaus <46694915+victorfeldhaus@users.noreply.github.com>
2022-05-07 11:54:50 -0300

    Update desafio02.js

--- desafio02.js
+++ desafio02.js
@@ -1,5 +1,3 @@
-// Passar o tempo de chegada de cada aluno no array tempoChegada
-// Passar o número de alunos para começar a aula no array limiteAlunos
 
 tempoChegada = [1 ,-3 , 4, -2, 0, -4, -10, 5, -7, 15, -20, -15, 7, -8, 9, -5]
 limiteAlunos = 10
